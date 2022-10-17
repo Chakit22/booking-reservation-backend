69bb608b43d8f5fc5adb1f68ab9d5aa27a083e06
Khushi Jain <khushij393@gmail.com>
2022-10-17 13:24:01 +0530

    env

--- api/index.js
+++ api/index.js
@@ -51,7 +51,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use(express.json()); /*This is an inbuilt method in express to recognize the incoming request as a JSON object.*/
-app.use(cookieParser());
+app.use(cookieParser())
 /*
 app.use((req,res,next) => {
   console.log("Hii Iam a middleware");
